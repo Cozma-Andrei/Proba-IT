ea9ad58b640749665e064b98889c916136e5747a
Cozma-Andrei <cozmaandrei1972@gmail.com>
2022-12-02 01:52:42 +0200

    first changes

---
+++ frontend.js
@@ -0,0 +1,29 @@
+function openForm1() {
+    document.getElementById("myForm1").style.display = "block";
+  }
+
+function openForm2() {
+    if (document.getElementById("button2").innerHTML === "Delogare") {
+    document.getElementById("button1").style.display = "block";
+    document.getElementById("button1").innerHTML = "Logare";
+    document.getElementById("button2").innerHTML = "Creare cont";
+    }
+
+    else
+    document.getElementById("myForm1").style.display = "block";
+}
+
+function closeForm1() {
+    document.getElementById("myForm1").style.display = "none";
+    document.getElementById("button2").innerHTML = "Delogare";
+    document.getElementById("button1").style.display = "none";
+  }
+
+function restoreButtons() {
+    document.getElementById("button1").innerHTML = "Logare";
+    document.getElementById("button2").innerHTML = "Creare Cont";
+}
+
+function goToUpload() {
+    document.getElementById("uploadMeme").scrollIntoView({behavior: 'smooth'});
+}
