e36a7f563bd85262fa9b0f6d83fba70bfb4a3e9f
Cozma-Andrei <cozmaandrei1972@gmail.com>
2022-12-03 01:34:03 +0200

    I made significant changes to the frontend

--- frontend.js
+++ frontend.js
@@ -1,16 +1,19 @@
 function openForm1() {
     document.getElementById("myForm1").style.display = "block";
+    document.getElementById("myForm2").style.display = "none";
   }
 
 function openForm2() {
     if (document.getElementById("button2").innerHTML === "Delogare") {
-    document.getElementById("button1").style.display = "block";
-    document.getElementById("button1").innerHTML = "Logare";
-    document.getElementById("button2").innerHTML = "Creare cont";
+      document.getElementById("button1").style.display = "block";
+      document.getElementById("button1").innerHTML = "Logare";
+      document.getElementById("button2").innerHTML = "Creare cont";
     }
 
-    else
-    document.getElementById("myForm1").style.display = "block";
+    else {
+      document.getElementById("myForm1").style.display = "none";
+      document.getElementById("myForm2").style.display = "block";
+    }
 }
 
 function closeForm1() {
@@ -19,6 +22,18 @@ function closeForm1() {
     document.getElementById("button1").style.display = "none";
   }
 
+function closeForm2() {
+    document.getElementById("myForm2").style.display = "none";
+  }
+
 function goToUpload() {
-    document.getElementById("uploadMeme").scrollIntoView({behavior: 'smooth'});
+    document.getElementById("uploadMeme").scrollIntoView({behavior: "smooth"});
+}
+
+function giveUpCompletion1(){
+  document.getElementById("myForm1").style.display = "none";
+}
+
+function giveUpCompletion2(){
+  document.getElementById("myForm2").style.display = "none";
 }
