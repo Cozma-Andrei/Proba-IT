6e9bacccecfc5a11b00441d53d2c57ff33b37f43
Cozma-Andrei <cozmaandrei1972@gmail.com>
2022-12-05 01:48:46 +0200

    Made changes to all files.

--- frontend.js
+++ frontend.js
@@ -1,39 +1,43 @@
 function openForm1() {
-    document.getElementById("myForm1").style.display = "block";
-    document.getElementById("myForm2").style.display = "none";
-  }
+  document.getElementById("myForm1").style.display = "block";
+  document.getElementById("myForm2").style.display = "none";
+}
 
 function openForm2() {
-    if (document.getElementById("button2").innerHTML === "Delogare") {
-      document.getElementById("button1").style.display = "block";
-      document.getElementById("button1").innerHTML = "Logare";
-      document.getElementById("button2").innerHTML = "Creare cont";
-    }
+  if (document.getElementById("button2").innerHTML === "Delogare")
+    window.location.reload();
 
-    else {
-      document.getElementById("myForm1").style.display = "none";
-      document.getElementById("myForm2").style.display = "block";
-    }
+  else {
+    document.getElementById("myForm1").style.display = "none";
+    document.getElementById("myForm2").style.display = "block";
+  }
 }
 
-function closeForm1() {
-    document.getElementById("myForm1").style.display = "none";
-    document.getElementById("button2").innerHTML = "Delogare";
-    document.getElementById("button1").style.display = "none";
-  }
+function modifyLabels() {
+  document.getElementById("myForm1").style.display = "none";
+  document.getElementById("button2").innerHTML = "Delogare";
+  document.getElementById("button1").style.display = "none";
+}
 
-function closeForm2() {
-    document.getElementById("myForm2").style.display = "none";
-  }
+function closeCreateForm() {
+  document.getElementById("myForm2").style.display = "none";
+}
 
 function goToUpload() {
-    document.getElementById("uploadMeme").scrollIntoView({behavior: "smooth"});
+  document.getElementById("rect1").scrollIntoView({behavior: "smooth"});
 }
 
-function giveUpCompletion1(){
+function giveUpCompletion1() {
   document.getElementById("myForm1").style.display = "none";
 }
 
-function giveUpCompletion2(){
+function giveUpCompletion2() {
   document.getElementById("myForm2").style.display = "none";
+}
+
+function alertNotLoggedIn() {
+  if (document.getElementById("button2").innerHTML === "Creare cont")
+    alert("Te rugăm să te autentifici înainte să trimiți un meme!");
+  else
+    alert("Meme-ul a fost trimis cu succes!");
 }
